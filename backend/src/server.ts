0f873b0a15e1ff148a087412cc303a55cb9b3780
Timothy Pokanai <pokanait@mcmaster.ca>
2026-02-13 19:05:26 -0500

    Configured MongoDB Atlas connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,18 @@
 import 'dotenv/config';
 import app from './app.js';
+import mongoose from 'mongoose';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
+const MONGO = process.env.MONGO_URI;
+
+// Connect to MongoDB
+if (!MONGO) {
+    console.error('MONGO_URI is not defined in .env');
+    process.exit(1);
+}
+mongoose.connect(MONGO)
+    .then(() => console.log(`Connected to MongoDB at ${MONGO}`))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 // Start the server
 app.listen(PORT, () => {
